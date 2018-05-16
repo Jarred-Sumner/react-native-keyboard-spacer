99c173000ccba2930ba681b2fcb521c147d19214
Jarred Sumner <jarred@jarredsumner.com>
2018-05-16 16:00:07 -0700

    Update KeyboardSpacer.js

--- KeyboardSpacer.js
+++ KeyboardSpacer.js
@@ -1,8 +1,8 @@
 /**
  * Created by andrewhurst on 10/5/15.
  */
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 import {
   Keyboard,
   LayoutAnimation,
@@ -11,14 +11,14 @@ import {
   ViewPropTypes,
   Platform,
   StyleSheet
-} from 'react-native';
+} from "react-native";
 
 const styles = StyleSheet.create({
   container: {
     left: 0,
     right: 0,
-    bottom: 0,
-  },
+    bottom: 0
+  }
 });
 
 // From: https://medium.com/man-moon/writing-modern-react-native-ui-e317ff956f02
@@ -39,12 +39,12 @@ export default class KeyboardSpacer extends Component {
   static propTypes = {
     topSpacing: PropTypes.number,
     onToggle: PropTypes.func,
-    style: ViewPropTypes.style,
+    style: ViewPropTypes.style
   };
 
   static defaultProps = {
     topSpacing: 0,
-    onToggle: () => null,
+    onToggle: () => null
   };
 
   constructor(props, context) {
@@ -59,8 +59,10 @@ export default class KeyboardSpacer extends Component {
   }
 
   componentDidMount() {
-    const updateListener = Platform.OS === 'android' ? 'keyboardDidShow' : 'keyboardWillShow';
-    const resetListener = Platform.OS === 'android' ? 'keyboardDidHide' : 'keyboardWillHide';
+    const updateListener =
+      Platform.OS === "android" ? "keyboardDidShow" : "keyboardWillShow";
+    const resetListener =
+      Platform.OS === "android" ? "keyboardDidHide" : "keyboardWillHide";
     this._listeners = [
       Keyboard.addListener(updateListener, this.updateKeyboardSpace),
       Keyboard.addListener(resetListener, this.resetKeyboardSpace)
@@ -77,46 +79,60 @@ export default class KeyboardSpacer extends Component {
     }
 
     let animationConfig = defaultAnimation;
-    if (Platform.OS === 'ios') {
+    if (Platform.OS === "ios") {
       animationConfig = LayoutAnimation.create(
         event.duration,
-        LayoutAnimation.Types[event.easing],
-        LayoutAnimation.Properties.opacity,
+        LayoutAnimation.Types.keyboard,
+        LayoutAnimation.Properties.opacity
       );
     }
     LayoutAnimation.configureNext(animationConfig);
 
     // get updated on rotation
-    const screenHeight = Dimensions.get('window').height;
+    const screenHeight = Dimensions.get("window").height;
     // when external physical keyboard is connected
     // event.endCoordinates.height still equals virtual keyboard height
     // however only the keyboard toolbar is showing if there should be one
-    const keyboardSpace = (screenHeight - event.endCoordinates.screenY) + this.props.topSpacing;
-    this.setState({
-      keyboardSpace,
-      isKeyboardOpened: true
-    }, this.props.onToggle(true, keyboardSpace));
+    const keyboardSpace =
+      screenHeight - event.endCoordinates.screenY + this.props.topSpacing;
+
+    this.spacerRef.setNativeProps({ height: keyboardSpace });
+    this.setState(
+      {
+        isKeyboardOpened: true
+      },
+      this.props.onToggle(true, keyboardSpace)
+    );
   }
 
   resetKeyboardSpace(event) {
     let animationConfig = defaultAnimation;
-    if (Platform.OS === 'ios') {
+    if (Platform.OS === "ios") {
       animationConfig = LayoutAnimation.create(
         event.duration,
-        LayoutAnimation.Types[event.easing],
-        LayoutAnimation.Properties.opacity,
+        LayoutAnimation.Types.keyboard,
+        LayoutAnimation.Properties.opacity
       );
     }
-    LayoutAnimation.configureNext(animationConfig);
+    // LayoutAnimation.configureNext(animationConfig);
 
-    this.setState({
-      keyboardSpace: 0,
-      isKeyboardOpened: false
-    }, this.props.onToggle(false, 0));
+    this.spacerRef.setNativeProps({ height: 0 });
+
+    this.setState(
+      {
+        keyboardSpace: 0,
+        isKeyboardOpened: false
+      },
+      this.props.onToggle(false, 0)
+    );
   }
 
   render() {
     return (
-      <View style={[styles.container, { height: this.state.keyboardSpace }, this.props.style]} />);
+      <View
+        ref={spacerRef => (this.spacerRef = spacerRef)}
+        style={[styles.container, this.props.style]}
+      />
+    );
   }
 }
